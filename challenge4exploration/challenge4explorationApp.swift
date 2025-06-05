11371a993704340d45873f6302256e6627bf8828
Daffa <muhammaddaffaashdaqfillah@gmail.com>
2025-06-05 14:13:17 +0700

    initial project

---
+++ challenge4exploration/challenge4explorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge4explorationApp.swift
+//  challenge4exploration
+//
+//  Created by Muhammad Daffa Ashdaqfillah on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct challenge4explorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
