4401677fa02a60d314964b0af4b714e2875eb505
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2025-06-17 18:16:31 +0700

    chore: asd

---
+++ challenge4exploration/MLKit/MLKitNew.swift
@@ -0,0 +1,18 @@
+//
+//  MLKitNew.swift
+//  challenge4exploration
+//
+//  Created by Ammar Alifian Fahdan on 17/06/25.
+//
+
+import SwiftUI
+
+struct MLKitNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MLKitNew()
+}
